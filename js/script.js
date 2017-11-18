ff9ab7883cdfaad6e31fd255607a006f5a237cd5
Arun <arunsubmail@gmail.com>
2017-11-18 17:38:12 +0530

    ajax call removed , wrongly placed letters solved, removed comments

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-// console.log("jsonFunction",jsonFunction);
-
 function fireSubmit() {
     clearSubmit();
     var sayUrName = document.getElementById('say-ur-name');
@@ -28,22 +26,23 @@ function processInput(input) {
         if (retVal === 'single letter') {
             retVal = matchElem(periodicArr, val);
         }
-        if (retVal !== null || retVal !== undefined) {
-            view.buildPeriodEl(retVal, idx);
-            var regex = '[^' + retVal.symbol + ']';
-            regex = new RegExp(regex, 'gi')
-            var reduceArr = val.match(regex);
-            reduceArr.splice(retVal.pos, 0, retVal);
-            // periodicArr.splice(periodicArr.indexOf(retVal), 1);
-            view.buildObjDom(reduceArr, retVal.pos, idx);
-            // view.updateColor(retVal, val, idx);
-        } else {
-            view.displayMessage('No match found');
+        if (retVal == null || retVal == undefined) {
+            view.displayMessage('No match found for this word: ' + val);
             clearForm();
             return;
+        } else {
+            view.buildPeriodEl(retVal, idx);
+            var reduceArr = getParts(retVal.symbol, val);
+            reduceArr.splice(retVal.pos, 0, retVal);
+            view.buildObjDom(reduceArr, retVal.pos, idx);
         }
     })
     clearForm();
+}
+
+function getParts(input, string) {
+    var regex = new RegExp(input, 'i')
+    return string.replace(regex, '').split('');
 }
 
 function matchElem(periodicArr, letterArr) {
@@ -51,8 +50,6 @@ function matchElem(periodicArr, letterArr) {
     for (var i = 0; i < letterArr.length; i++) {
         for (var j = 0; j < periodicArr.length; j++) {
             if (letterArr[i] === periodicArr[j].symbol) {
-                // if (letterArr[i] === periodicArr[j]) {
-                // return letterArr[i];
                 periodicArr[j]['pos'] = i;
                 return periodicArr[j];
             } else if ((letterArr[letterArr.length - 1].length === model.chunkNumber) && (periodicArr.length - 1) === j && (letterArr.length - 1) === i) {
@@ -90,24 +87,6 @@ function init() {
     ajaxCall();
 }
 
-// function forFn(arr) {
-//     var retArray = [];
-//     for (var i = 0; i < arr.length; i++) {
-//         if (arr[i].symbol) {
-//             var elObj = {
-//                 atomic_mass: arr[i].atomic_mass,
-//                 number: arr[i].number,
-//                 shells: arr[i].shells,
-//                 symbol: arr[i].symbol.toLowerCase()
-//             }
-//             retArray.push(elObj);
-//         } else {
-//             retArray.push(arr[i].toLowerCase());
-//         }
-//     }
-//     return retArray;
-// }
-
 window.onload = init;
 
 var model = {
@@ -125,11 +104,11 @@ var view = {
         }
     },
     buildPeriodEl: function(obj, index) {
-        // build for first element then depend on index build on second element distance from previous
         var inputgroup = $('.input-group');
         var inputword = "input-word-" + index;
         var genWordId = '#' + inputword;
         inputgroup.append('<div id=' + inputword + ' style="margin-left:' + view.marginLeft + 'px"></div>');
+        var details = $('.details');
         view.marginLeft = 0;
         $(genWordId).append('<div class="periodic-element"><div class="atomic-mass">' + obj.atomic_mass + '</div><div class="oxidation"></div><div class="symbol">' + obj.symbol.toLowerCase() + '</div><div class="number">' + obj.number + '</div><div class="shells">2</div></div>');
         obj.shells.forEach(function(val, idx) {
@@ -138,6 +117,7 @@ var view = {
         obj.oxidationStates.forEach(function(val, idx) {
             $(genWordId + ' ' + '.oxidation').append("<span>" + val + "</span>");
         });
+        details.append('<div>' + obj.name + '----------' + obj.source + '----------' + obj.summary + '</div>');
         var perEl = $('.periodic-element').outerWidth();
         view.marginLeft += perEl;
     },
@@ -145,28 +125,8 @@ var view = {
         var inputgroup = $('.input-group');
         var inputword = "input-word-" + index;
         var genWordId = '#' + inputword;
-        var details = $('.details');
-        // inputgroup.append('<div id=' + inputword + '></div>');
         var length = dom.length - 1;
-        for (var i = length; i >= 0 ; i--) {
-            // if (dom[i].symbol) {
-            //     $(genWordId).append('<div class="periodic-element"><div class="atomic-mass">' + dom[i].atomic_mass + '</div><div class="oxidation"></div><div class="symbol">' + dom[i].symbol.toLowerCase() + '</div><div class="number">' + dom[i].number + '</div><div class="shells">2</div></div>');
-            //     dom[i].shells.forEach(function(val, idx) {
-            //         $(genWordId + ' ' + '.shells').append("<span>" + '-' + val + "</span>");
-            //     });
-            //     dom[i].oxidationStates.forEach(function(val, idx) {
-            //         $(genWordId + ' ' + '.oxidation').append("<span>" + val + "</span>");
-            //     });
-            //     details.append('<div>'+dom[i].name+'----------'+dom[i].source+'----------'+dom[i].summary+'</div>');
-            //     console.log("big",$('.periodic-element').outerWidth());
-            //     var perEl = $('.periodic-element').outerWidth();
-            //     count += perEl;
-            // } else {
-            //     $(genWordId).append("<span>" + dom[i].toLowerCase() + "</span>");
-            //     console.log("span",$(genWordId + ' > span').outerWidth());
-            //     var spanEl = $(genWordId + ' > span').outerWidth();
-            //     count += spanEl;
-            // }
+        for (var i = length; i >= 0; i--) {
             if (!dom[i].symbol) {
                 if (i < pos) {
                     $(genWordId).prepend("<span>" + dom[i].toLowerCase() + "</span>");
@@ -176,8 +136,8 @@ var view = {
             }
         }
 
-        
-            for (var i = 0; i < dom.length; i++) {
+
+        for (var i = 0; i < dom.length; i++) {
             if (!dom[i].symbol) {
                 if (i > pos) {
                     $(genWordId).append("<span>" + dom[i].toLowerCase() + "</span>");
@@ -192,68 +152,24 @@ var view = {
         message.text(str).fadeOut(4000, function() {
             message.show().text("");
         });
-    },
-    updateColor: function(colorEle, splitWord, idx) {
-        var name = $('#name');
-        var wordId = "word_" + idx
-        var genWordId = '#' + wordId;
-        if (idx > 0) {
-            name.append('<div id=' + wordId + '></div>');
-            for (var i = 0; i < splitWord.length; i++) {
-                $(genWordId).append("<span>" + splitWord[i].toLowerCase() + "</span>");
-            }
-        } else {
-            this.buildDom(splitWord);
-        }
-        if (colorEle === undefined) {
-            return;
-        } else if (colorEle.length === 2) {
-            colorEle = colorEle.match(/.{1,1}/g);
-        }
-
-        for (var i = 0; i < colorEle.length; i++) {
-            for (var k = 0; k < splitWord.length; k++) {
-                if (splitWord[k] === colorEle[i]) {
-
-                    if (idx > 0) {
-                        $(genWordId).children().eq(i).addClass('highlight');
-                    } else {
-                        name.children().eq(i).addClass('highlight');
-                    }
-                    break;
-                }
-            }
-
-        }
     }
 };
 
 var controller = {
-    jsonUrl: 'PeriodicTableJSON.json',
-    // processInput: function(colorList) {
-    //     view.updateColor(colorList, model.origInput);
-    // }
+    jsonUrl: 'PeriodicTableJSON.json'
 };
 
 function ajaxCall() {
-    $.ajax({
-        dataType: "json",
-        url: controller.jsonUrl,
-        data: "data",
-        success: function(response) {
-            // model.jsonElems = forFn(response.elements);
-            model.jsonElems = response.elements.map(function(obj) {
-                return {
-                    name: obj.name,
-                    source: obj.source,
-                    summary: obj.summary,
-                    atomic_mass: obj.atomic_mass,
-                    number: obj.number,
-                    shells: obj.shells,
-                    symbol: obj.symbol.toLowerCase(),
-                    oxidationStates: obj.oxidationStates
-                };
-            });
-        }
+    model.jsonElems = jsonData.elements.map(function(obj) {
+        return {
+            name: obj.name,
+            source: obj.source,
+            summary: obj.summary,
+            atomic_mass: obj.atomic_mass,
+            number: obj.number,
+            shells: obj.shells,
+            symbol: obj.symbol.toLowerCase(),
+            oxidationStates: obj.oxidationStates
+        };
     });
 }
