23aef6ed8f6fb118f92e9daeecbedb293bbc2128
Arun <arunsubmail@gmail.com>
2017-12-26 13:06:15 +0530

    deleted fonts, dilemma in animate call, addded spec file

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ function fireSubmit() {
 
 function processInput(input) {
     var splitInput = input.split(/\s+/);
-    var periodicArr = model.jsonElems;
+    var periodicArr = ajaxCall();
     btnState("disabled");
     for (var i = 0; i < splitInput.length; i++) {
         var retVal = regexMatch(periodicArr, splitInput[i]);
@@ -31,19 +31,20 @@ function processInput(input) {
         }
     }
     $('.periodic-element').addClass('pseudo').css("transition-delay", view.get('delay') + "s");
+    $('.details').css("transition-delay", view.get('delay') +1+ "s").fadeIn();
     clearForm();
     return;
 }
 
 function joinArray(str, retVal) {
-    var first = getParts(retVal.symbol, str);
+    var first = replaceSymbol(retVal.symbol, str);
     var removed = { string: first.splice(retVal.pos).join(''), after: true };
     first = { string: first.join(''), before: true };
     var retArr = Array.of(first, retVal, removed);
     return retArr;
 }
 
-function getParts(input, string) {
+function replaceSymbol(input, string) {
     var regex = new RegExp(input, 'i')
     return string.replace(regex, '').split('');
 }
@@ -85,7 +86,7 @@ function init() {
     var refresh = document.getElementById('refresh');
     submitBtn.onclick = fireSubmit;
     refresh.onclick = refreshPage;
-    ajaxCall();
+
 }
 
 window.onload = init;
@@ -93,6 +94,10 @@ window.onload = init;
 var model = {
     jsonElems: []
 };
+
+function foreachtime(item) {
+    
+}
 
 var view = {
     left: 0,
@@ -108,30 +113,45 @@ var view = {
         var inputword = "input-word-" + index;
         var genWordId = '.' + inputword;
         var details = $('.details');
-        var length = dom.length - 1;
         inputgroup.append('<div class=' + inputword + '><div class="periodic-element" style="left:' + view.get('left') + 'px;transition-delay:' + view.get('delay') + 's"></div></div>');
 
-        for (var i = 0; i < dom.length; i++) {
+        // for (var i = 0; i < dom.length; i++) {
 
-            if (dom[i].symbol) {
-                $(genWordId + ' ' + '.periodic-element').append('<div class="atomic-mass">' + dom[i].atomic_mass + '</div><div class="oxidation"></div><div class="symbol">' + dom[i].symbol + '</div><div class="number">' + dom[i].number + '</div><div class="shells">2</div>');
-                dom[i].shells.forEach(function(val) {
-                    $(genWordId + ' ' + '.shells').append("<span>" + '-' + val + "</span>");
-                });
-                dom[i].oxidationStates.forEach(function(val) {
-                    $(genWordId + ' ' + '.oxidation').append("<span>" + val + "</span>");
-                });
-                details.append('<div>' + dom[i].name + '----------' + dom[i].source + '----------' + dom[i].summary + '</div>');
-            }else if (dom[i].before) {
-                $(genWordId + ' ' + '.periodic-element').attr("data-before", dom[i].string);
-                var spanEl = $(genWordId + ' .periodic-element').outerWidth();
-            } else if (dom[i].after) {
-                $(genWordId + ' ' + '.periodic-element').attr("data-after", dom[i].string);
-            }
+        //     if (dom[i].symbol) {
+        //         $(genWordId + ' ' + '.periodic-element').append('<div class="atomic-mass">' + dom[i].atomic_mass + '</div><div class="oxidation"></div><div class="symbol">' + dom[i].symbol + '</div><div class="number">' + dom[i].number + '</div><div class="shells">2</div>');
+        //         dom[i].shells.forEach(function(val) {
+        //             $(genWordId + ' ' + '.shells').append("<span>" + '-' + val + "</span>");
+        //         });
+        //         dom[i].oxidationStates.forEach(function(val) {
+        //             $(genWordId + ' ' + '.oxidation').append("<span>" + val + "</span>");
+        //         });
+        //         details.append('<div>' + dom[i].name + '----------' + dom[i].source + '----------' + dom[i].summary + '</div>');
+        //     }else if (dom[i].before) {
+        //         $(genWordId + ' ' + '.periodic-element').attr("data-before", dom[i].string);
+        //         var spanEl = $(genWordId + ' .periodic-element').outerWidth();
+        //     } else if (dom[i].after) {
+        //         $(genWordId + ' ' + '.periodic-element').attr("data-after", dom[i].string);
+        //     }
 
-            $(genWordId + ' ' + '.periodic-element').addClass('animate');
-        }
-        var el = $(genWordId + ' .periodic-element').outerWidth()
+        //     $(genWordId + ' ' + '.periodic-element').addClass('animate');
+        // }
+
+        $(genWordId + ' ' + '.periodic-element').append('<div class="atomic-mass">' + dom[1].atomic_mass + '</div><div class="oxidation"></div><div class="symbol">' + dom[1].symbol + '</div><div class="number">' + dom[1].number + '</div><div class="shells">2</div>');
+        dom[1].shells.forEach(function(val) {
+            $(genWordId + ' ' + '.shells').append("<span>" + '-' + val + "</span>");
+        });
+        dom[1].oxidationStates.forEach(function(val) {
+            $(genWordId + ' ' + '.oxidation').append("<span>" + val + "</span>");
+        });
+        details.append('<div>' + dom[1].name + '----------' + dom[1].source + '----------' + dom[1].summary + '</div>');
+
+        $(genWordId + ' ' + '.periodic-element').attr("data-before", dom[0].string);
+        $(genWordId + ' ' + '.periodic-element').attr("data-after", dom[2].string);
+        // var spanEl = $(genWordId + ' .periodic-element').outerWidth();
+
+        $(genWordId + ' ' + '.periodic-element').addClass('animate');
+
+        var el = $(genWordId + ' .periodic-element').outerWidth();
         view.set('left', el);
         view.set('delay', 2.5);
     },
@@ -144,7 +164,7 @@ var view = {
 };
 
 function ajaxCall() {
-    model.jsonElems = jsonData.elements.map(function(obj) {
+    return tempObj = jsonData.elements.map(function(obj) {
         return {
             name: obj.name,
             source: obj.source,
