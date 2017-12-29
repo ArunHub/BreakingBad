07075441434e965579cd1b867e2f7316e3470467
Arun <arunsubmail@gmail.com>
2017-12-29 08:53:42 +0530

    removed (removed,first prop in joinarray fn to string), assigned var for genworld periodicel and changed onclick to html since it disturbes testing and some specs added to test

--- js/jasmine2.8.0/spec/script.spec.js
+++ js/jasmine2.8.0/spec/script.spec.js
@@ -1,27 +1,47 @@
 describe('jasmine-node', function(){
 
-  var df = "";
+  
 
   it('should break string into array items with given count', function(){
-    // expect(df).toEqual(3);
+    var breakSt = breakStr("breaking",3);
+    expect(breakSt).toEqual(["bre", "rea", "eak", "aki", "kin", "ing"]);
   });
 
-  it('shows asynchronous test', function(){
-    // setTimeout(function(){
-    //   expect('second').toEqual('second');
-    //   asyncSpecDone();
-    // }, 1);
-    // expect('first').toEqual('first');
-    // asyncSpecWait();
+  it('should replace symbol with empty', function(){
+    var replace = replaceSymbol("br","breaking");
+    expect(replace).toEqual(["e", "a", "k", "i", "n", "g"]);
+  });
+  it('should find regex match of element', function(){
+    var periodicArr = ajaxCall();
+    var regex_match = regexMatch(periodicArr, "br");
+    expect(regex_match).not.toBe(null);
+    expect(regex_match).toEqual(jasmine.objectContaining({
+      symbol: "br"
+    }));
+  });  
+  it('should join array of three', function(){
+    var periodicArr = ajaxCall();
+    var join_array = joinArray("breaking", {symbol: "br",pos: 0});
+    expect(join_array).not.toBe(null);
+    expect(join_array).toContain("eaking");
   });
 
-  it('shows asynchronous test node-style', function(done){
-    // setTimeout(function(){
-    //   expect('second').toEqual('second');
-    //   // If you call done() with an argument, it will fail the spec 
-    //   // so you can use it as a handler for many async node calls
-    //   done();
-    // }, 1);
-    // expect('first').toEqual('first');
+  xit('shows asynchronous test', function(){
+    setTimeout(function(){
+      expect('second').toEqual('second');
+      asyncSpecDone();
+    }, 1);
+    expect('first').toEqual('first');
+    asyncSpecWait();
+  });
+
+  xit('shows asynchronous test node-style', function(done){
+    setTimeout(function(){
+      expect('second').toEqual('second');
+      // If you call done() with an argument, it will fail the spec 
+      // so you can use it as a handler for many async node calls
+      done();
+    }, 1);
+    expect('first').toEqual('first');
   });
 });
--- js/script.js
+++ js/script.js
@@ -38,8 +38,8 @@ function processInput(input) {
 
 function joinArray(str, retVal) {
     var first = replaceSymbol(retVal.symbol, str);
-    var removed = { string: first.splice(retVal.pos).join(''), after: true };
-    first = { string: first.join(''), before: true };
+    var removed = first.splice(retVal.pos).join('');
+    first = first.join('');
     var retArr = Array.of(first, retVal, removed);
     return retArr;
 }
@@ -81,23 +81,18 @@ function refreshPage() {
     window.location.reload();
 }
 
-function init() {
-    var submitBtn = document.getElementById('submitBtn');
-    var refresh = document.getElementById('refresh');
-    submitBtn.onclick = fireSubmit;
-    refresh.onclick = refreshPage;
+// function init() {
+//     var submitBtn = document.getElementById('submitBtn');
+//     var refresh = document.getElementById('refresh');
+//     submitBtn.onclick = fireSubmit;
+//     refresh.onclick = refreshPage;
+// }
 
-}
-
-window.onload = init;
+// window.onload = init;
 
 var model = {
     jsonElems: []
 };
-
-function foreachtime(item) {
-    
-}
 
 var view = {
     left: 0,
@@ -114,11 +109,12 @@ var view = {
         var genWordId = '.' + inputword;
         var details = $('.details');
         inputgroup.append('<div class=' + inputword + '><div class="periodic-element" style="left:' + view.get('left') + 'px;transition-delay:' + view.get('delay') + 's"></div></div>');
+        var _this = $(genWordId + ' .periodic-element');
 
         // for (var i = 0; i < dom.length; i++) {
 
         //     if (dom[i].symbol) {
-        //         $(genWordId + ' ' + '.periodic-element').append('<div class="atomic-mass">' + dom[i].atomic_mass + '</div><div class="oxidation"></div><div class="symbol">' + dom[i].symbol + '</div><div class="number">' + dom[i].number + '</div><div class="shells">2</div>');
+        //         $(genWordId + ' .periodic-element').append('<div class="atomic-mass">' + dom[i].atomic_mass + '</div><div class="oxidation"></div><div class="symbol">' + dom[i].symbol + '</div><div class="number">' + dom[i].number + '</div><div class="shells">2</div>');
         //         dom[i].shells.forEach(function(val) {
         //             $(genWordId + ' ' + '.shells').append("<span>" + '-' + val + "</span>");
         //         });
@@ -127,16 +123,16 @@ var view = {
         //         });
         //         details.append('<div>' + dom[i].name + '----------' + dom[i].source + '----------' + dom[i].summary + '</div>');
         //     }else if (dom[i].before) {
-        //         $(genWordId + ' ' + '.periodic-element').attr("data-before", dom[i].string);
+        //         $(genWordId + ' .periodic-element').attr("data-before", dom[i].string);
         //         var spanEl = $(genWordId + ' .periodic-element').outerWidth();
         //     } else if (dom[i].after) {
-        //         $(genWordId + ' ' + '.periodic-element').attr("data-after", dom[i].string);
+        //         $(genWordId + ' .periodic-element').attr("data-after", dom[i].string);
         //     }
 
-        //     $(genWordId + ' ' + '.periodic-element').addClass('animate');
+        //     $(genWordId + ' .periodic-element').addClass('animate');
         // }
 
-        $(genWordId + ' ' + '.periodic-element').append('<div class="atomic-mass">' + dom[1].atomic_mass + '</div><div class="oxidation"></div><div class="symbol">' + dom[1].symbol + '</div><div class="number">' + dom[1].number + '</div><div class="shells">2</div>');
+        _this.append('<div class="atomic-mass">' + dom[1].atomic_mass + '</div><div class="oxidation"></div><div class="symbol">' + dom[1].symbol + '</div><div class="number">' + dom[1].number + '</div><div class="shells">2</div>');
         dom[1].shells.forEach(function(val) {
             $(genWordId + ' ' + '.shells').append("<span>" + '-' + val + "</span>");
         });
@@ -145,13 +141,13 @@ var view = {
         });
         details.append('<div>' + dom[1].name + '----------' + dom[1].source + '----------' + dom[1].summary + '</div>');
 
-        $(genWordId + ' ' + '.periodic-element').attr("data-before", dom[0].string);
-        $(genWordId + ' ' + '.periodic-element').attr("data-after", dom[2].string);
-        // var spanEl = $(genWordId + ' .periodic-element').outerWidth();
+        _this.attr("data-before", dom[0]);
+        _this.attr("data-after", dom[2]);
+        // var spanEl = _this.outerWidth();
 
-        $(genWordId + ' ' + '.periodic-element').addClass('animate');
+        _this.addClass('animate');
 
-        var el = $(genWordId + ' .periodic-element').outerWidth();
+        var el = _this.outerWidth();
         view.set('left', el);
         view.set('delay', 2.5);
     },
