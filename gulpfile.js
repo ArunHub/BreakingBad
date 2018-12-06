90867ab74b562fbc847558301ecc817594ddc002
Arun <arunsubmail@gmail.com>
2018-12-06 12:46:23 +0530

    commiting in between on urgent

--- gulpfile.js
+++ gulpfile.js
@@ -1,71 +1,81 @@
 var gulp = require('gulp');
-const jasmine = require('gulp-jasmine');
 var browserSync = require('browser-sync');
-var runSequence = require('run-sequence');
-const reporters = require('jasmine-reporters');
+var runSequence = require('run-sequence').use(gulp);
 var htmlmin = require('gulp-html-minifier');
 const cssnano = require('gulp-cssnano');
 var uglify = require('gulp-uglify');
 var pump = require('pump');
 var concat = require('gulp-concat');
- 
-gulp.task('pack-js', function () {  
-  return gulp.src(['js/modernizr.js', 'js/json.js', 'js/script.js'])
-    .pipe(concat('bundle.js'))
-    .pipe(uglify({
-      ie8: false,
-    }))
-    .pipe(gulp.dest('public/build/js'));
-});
- 
-gulp.task('pack-css', function () { 
-  return gulp.src('./css/*.css')
-    .pipe(concat('stylesheet.css'))
-    .pipe(cssnano({
-      discardDuplicates: true,
-      discardEmpty: true
-    }))
-   .pipe(gulp.dest('public/build/css'));
-});
-// gulp.task('default', ['pack-js', 'pack-css']);
+const imagemin = require('gulp-imagemin');
+var jasmineBrowser = require('gulp-jasmine-browser');
+var protractor = require("gulp-protractor").protractor;
 
-gulp.task('test', function() {
-    return gulp.src('./test/test.js')
-        // gulp-jasmine works on filepaths so you can't have any plugins before it
-        .pipe(jasmine({
-            reporter: new reporters.JUnitXmlReporter()
+//packing js
+gulp.task('pack-js', function() {
+    return gulp.src(['js/modernizr.js', 'js/smoke-emitter.js', 'js/json.js', 'js/script.js'])
+        .pipe(concat('bundle.js'))
+        .pipe(uglify({
+            ie8: false,
         }))
+        .pipe(gulp.dest('dist/js'));
 });
 
-gulp.task('uglify', function(cb) {
-  pump([
-        gulp.src('js/script.js'),
-        uglify(),
-        gulp.dest('dist')
-    ],
-    cb
-  );
+gulp.task('e2e', function(done) {
+    gulp.src(__dirname + './e2e/')
+        .pipe(protractor({
+            configFile: './protractor.config.js',
+            args: ['--baseUrl', 'http://127.0.0.1:8000']
+        }))
+        .on('error', function(e) { throw e })
 });
 
+//unit test
+gulp.task('jasmine', function() {
+    return gulp.src(['test/**/*.js'])
+        .pipe(jasmineBrowser.specRunner())
+        .pipe(jasmineBrowser.server({ port: 8888 }));
+});
+
+//packing css
+gulp.task('pack-css', function() {
+    return gulp.src('./css/*.css')
+        .pipe(concat('stylesheet.css'))
+        .pipe(cssnano({
+            discardDuplicates: true,
+            discardEmpty: true
+        }))
+        .pipe(gulp.dest('dist/css'));
+});
+
+//minify html
 gulp.task('minify', function() {
-  gulp.src('./index.html')
-    .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('./public/build'))
+    gulp.src('./index.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('./dist'))
 });
 
-// gulp.task('cssnano', function() {
-//     return gulp.src('./css/*.css')
-//         .pipe(cssnano())
-//         .pipe(gulp.dest('./dist'));
-// });
+//copy fonts
+gulp.task('fonts', function() {
+    gulp.src('./css/fonts/*')
+        .pipe(gulp.dest('./dist/css/fonts'))
+});
 
+//optimize images
+gulp.task('imagemin', () =>
+    gulp.src('./images/*')
+    .pipe(imagemin({ progressive: true }))
+    .pipe(gulp.dest('dist/images'))
+);
+
+
+//build all
 gulp.task('build', function(callback) {
-    runSequence(['browserSync', 'cssnano'],
+    runSequence(['minify', 'imagemin', 'pack-css', 'fonts', 'pack-js'],
         callback
     )
 })
 
-
+//watch all changes
 gulp.task('watch', ['browserSync'], function() {
     // Reloads the browser whenever HTML or JS files change
     gulp.watch('./*.html', browserSync.reload);
@@ -73,6 +83,7 @@ gulp.task('watch', ['browserSync'], function() {
     gulp.watch('./css/*.css', browserSync.reload);
 });
 
+//spins the browser for changes
 gulp.task('browserSync', function() {
     browserSync({
         server: {
@@ -81,6 +92,7 @@ gulp.task('browserSync', function() {
     })
 })
 
+//default gulp run
 gulp.task('default', function(callback) {
     runSequence(['browserSync', 'watch'],
         callback
