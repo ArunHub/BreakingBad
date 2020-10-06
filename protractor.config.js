caa09a72d0d6d715b187ce19830d08a52efbc7b3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-06 06:23:20 +0000

    Deploy ArunHub/BreakingBad to github.com/ArunHub/BreakingBad.git:gh-pages

--- protractor.config.js
+++
@@ -1,3 +0,0 @@
-seleniumServerJar: './node_modules/protractor/selenium/selenium-server-standalone-2.42.0.jar',
-chromeDriver: './node_modules/protractor/selenium/chromedriver.exe',
-//seleniumAddress: 'http://localhost:4444/wd/hub',
--- vendor.js
+++
@@ -1,4 +0,0 @@
-require('./js/modernizr');
-require('./js/json');
-require('./js/smoke-emitter');
-require('./css/style.css')
